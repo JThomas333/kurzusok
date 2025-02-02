2575babd0a41db11b35b1fe4d7f309df0b19175c
JThomas333 <98952709+JThomas333@users.noreply.github.com>
2025-02-02 18:56:34 +0100

    Add files via upload

--- kurz.js
+++ kurz.js
@@ -1,7 +1,6 @@
 const alapUrl = "https://vvri.pythonanywhere.com/api/courses";
 
-let KorT = ""; //courses, students
-
+let KorT = ""; // courses, students
 
 function fetchItems() {
     let url = alapUrl;
@@ -27,8 +26,10 @@ function adatok(items) {
         const card = document.createElement("div");
         card.classList.add("card");
         card.innerHTML = `
-        <h3>${i.name}</h3>
+        <h3 id="sznev${i.id}" contenteditable="false">${i.name}</h3>  
+        <button onclick="szerkvan(${i.id})">Szerkesztés</button>  
         <button onclick="torles(${i.id})">Törlés</button>
+        <button onclick="szerkment(${i.id})" style="display:none;" id="save${i.id}">Mentés</button>  
         `;
         container.appendChild(card);
     });
@@ -42,7 +43,6 @@ function kurzus() {
 function tanulo() {
     KorT = "students";
     fetchItems();
-
 }
 
 //Új kurzus vagy diák hozzáadása
@@ -58,12 +58,12 @@ function ujtanulo() {
 
 function ujitas() {
     const ujadat = document.getElementById("ujadatok");
-    ujadat.style.display = "block";//Megjelenítjük az adat bekérést
+    ujadat.style.display = "block"; //Megjelenítjük az adat bekérést
 }
 
 function megse() {
     const ujadat = document.getElementById("ujadatok");
-    ujadat.style.display = "none";//Bezárjuk az adat bekérést
+    ujadat.style.display = "none"; //Bezárjuk az adat bekérést
 }
 
 function mentes() {
@@ -88,8 +88,45 @@ function mentes() {
     .then(response => response.json())
     .then(data => {
         console.log("Új adat hozzáadva!", data);
-        fetchItems();//Frissítjük a listát
+        fetchItems(); //Frissítjük a listát
         megse();
+    })
+    .catch(error => {név
+        console.error("Hiba történt!", error);
+    });
+}
+
+// Szerkesztés gomb aktiválása
+function szerkvan(id) {
+    const nevelem = document.getElementById(`sznev${id}`);
+    nevelem.contentEditable = true;  
+    document.getElementById(`save${id}`).style.display = "block";  
+}
+
+// Szerkesztett kurzus vagy diák mentése
+function szerkment(id) {
+    const name = document.getElementById(`sznev${id}`).innerText; 
+
+    const adathalmaz = {
+        name: name
+    };
+
+    let url = alapUrl;
+    if (KorT == "students") {
+        url = "https://vvri.pythonanywhere.com/api/students";
+    }
+
+    fetch(`${url}/${id}`, {  
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(adathalmaz),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Adat frissítve!", data);
+        fetchItems(); //Frissítjük a listát
     })
     .catch(error => {
         console.error("Hiba történt!", error);
@@ -102,13 +139,13 @@ function torles(id) {
         url = "https://vvri.pythonanywhere.com/api/students";
     }
 
-    fetch(`${url}/${id}`, {//kurzus/5
+    fetch(`${url}/${id}`, { //kurzus/5
         method: "DELETE"
     })
     .then(response => response.json())
     .then(data => {
         console.log("Elem törölve:", data);
-        fetchItems();//Frissítjük a listát
+        fetchItems(); //Frissítjük a listát
     })
     .catch(error => {
         console.error("Hiba történt!", error);
